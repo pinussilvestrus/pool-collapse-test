fb8d45d4d3ac353669861d47a6f29648b60a3e4e
Niklas Kiefer <niklas.kiefer@camunda.com>
2020-12-01 12:06:01 +0100

    chore: move from parcel to webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  devServer: {
+    contentBase: '.',
+    compress: true,
+    port: 9000
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.bpmn$/i,
+        use: 'raw-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        use: 'file-loader'
+      }
+    ]
+  }
+};
